e178b09b25dd8f42c0353f7f2be96d392a2d5305
Edgar <davidnavarro81230@outlook.com>
2023-09-18 22:02:23 -0500

    Scroll bar menu

--- src/components/SearchExercises.js
+++ src/components/SearchExercises.js
@@ -1,17 +1,29 @@
 import React, {useEffect, useState} from 'react';
 import {Box, Button, Stack, TextField, Typography} from '@mui/material';
 import { exerciseOptions, fetchData } from '../utils/fetchData';
+import HorizontalScrollbar from './HorizontalScrollbar';
 
-const SearchExercises = () => {
+const SearchExercises = ({setExercises, bodyPart, setBodyPart}) => {
   const [search, setSearch] = useState('');
-  const [exercises, setExercises] = useState([]);
+  const [bodyParts, setBodyParts] = useState([]);
+
+  useEffect(() => {
+    const fetchExercisesData = async () => {
+      const bodyPartsData = await fetchData('https://exercisedb.p.rapidapi.com/exercises/bodyPartList', exerciseOptions);
+
+      setBodyParts(['all', ...bodyPartsData]);
+
+    }
+
+    fetchExercisesData();
+  }, [])
 
   const handleSearch = async () => {
     if(search) {
-      const exerciseData = await fetchData('https://exercisedb.p.rapidapi.com/exercises/bodyPartList',
+      const exercisesData = await fetchData('https://exercisedb.p.rapidapi.com/exercises',
       exerciseOptions);
 
-      const searchedExercises = exerciseData.filter((exercise) => 
+      const searchedExercises = exercisesData.filter((exercise) => 
       exercise.name.toLowerCase().includes(search) 
       || exercise.bodyPart.toLowerCase().includes(search)
       || exercise.target.toLowerCase().includes(search)
@@ -56,6 +68,9 @@ const SearchExercises = () => {
           >
           Search
         </Button>
+      </Box>
+      <Box sx={{position: 'relative', width: '100%', p: '20px'}}>
+        <HorizontalScrollbar data={bodyParts} bodyPart={bodyPart} setBodyPart={setBodyPart}/>
       </Box>
     </Stack>
   )
